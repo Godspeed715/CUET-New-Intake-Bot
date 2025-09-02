2e24bb78cb0c59568971a5bd42c51406a108adeb
Godspeed715 <chimzagamdaniel@gmail.com>
2025-09-02 16:41:38 +0100

    modified bot.py
    added added a question

--- bot.py
+++ bot.py
@@ -1,19 +1,28 @@
 from typing import Final
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler
 
 TOKEN: Final = "8175830217:AAGLvST7qsqZXXfaoiq0oGZ5aTJFyglHfC4"
 BOT_USERNAME: Final = "@CUET_NEW_INTAKE_BOT"
-
+CUET_REGISTRATION_FORM = "https://forms.gle/d7CdSUdVdwYwJB397"
+CUET_GC_LINK = "https://t.me/+Nx2jAnLnH40wNzdk"
 
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard_1 = [[InlineKeyboardButton("CUET Registration Form", url=CUET_REGISTRATION_FORM)]]
+    keyboard_2 = [
+        [InlineKeyboardButton("Yes", callback_data="yes"), InlineKeyboardButton("No", callback_data="no")]
+    ]
+    reply_markup_1 = InlineKeyboardMarkup(keyboard_1)
+    reply_markup_2 = InlineKeyboardMarkup(keyboard_2)
+
+
     await update.message.reply_text("Hello there😊! You must be aspiring to join "
                                     "CUET (Covenant University Evangelical Team)😁.")
     await update.message.chat.send_message("Please do me a favour and fill the form below😉."
                                             "Also at the end of the form you'll see the link"
-                                           "to our telegram GC.")
-    await update.message.chat.send_message("")
+                                           "to our telegram GC.", reply_markup=reply_markup_1)
+    await update.message.chat.send_message("Are you done with filling the form?", reply_markup=reply_markup_2)
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -22,8 +31,19 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def reset_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(" ")
 
+async def button_handler_1(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()  # Acknowledge button press
+
+    if query.data == "yes":
+        await query.edit_message_text("So now onto the next Question 👇")
+
 # Responses
 
+
+
+# self commands
+# async def service_unit(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 # error
@@ -37,7 +57,7 @@ if __name__ == '__main__':
     # Commands
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
-    app.add_handler(CommandHandler('start', reset_command))
+    app.add_handler(CallbackQueryHandler(button_handler_1))
 
 
     # error handler
