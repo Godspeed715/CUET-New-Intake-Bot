cff1cc00a575752808c9961a5da7d971f76e6b3e
Godspeed715 <chimzagamdaniel@gmail.com>
2025-09-02 15:50:48 +0100

    modified bot.py
    added a few things

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from typing import Final
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN: Final = "8175830217:AAGLvST7qsqZXXfaoiq0oGZ5aTJFyglHfC4"
 BOT_USERNAME: Final = "@CUET_NEW_INTAKE_BOT"
 
-form_message  = ()
 
+# Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello there😊! You must be aspiring to join "
                                     "CUET (Covenant University Evangelical Team)😁.")
@@ -17,7 +17,32 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text()
+    await update.message.reply_text("This is a bot to join CUET (Covenant University Evangelical Team).")
 
 async def reset_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text()
+    await update.message.reply_text(" ")
+
+# Responses
+
+
+
+# error
+async def error(update: Update, context:ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('start', reset_command))
+
+
+    # error handler
+    app.add_error_handler(error)
+
+    # Polls bot
+    print('Polling...')
+    app.run_polling(poll_interval=3)
